bb390a447f658a3a2500ab1f15ea542f71cce6d1
Cassandra <cassandra.moore1516@gmail.com>
2025-06-06 19:19:46 -0500

    created utils.js file

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+export function usedFunction() {
+  return "This function is used!";
+}
+
+export function unusedFunction() {
+  return "This function won't be used!";
+}
+
+export function anotherUnusedFunction() {
+  return "This function is also unused!";
+}
