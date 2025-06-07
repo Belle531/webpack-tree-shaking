865cfcf9f6c550ba77001357b7dd3d45ec054453
Cassandra <cassandra.moore1516@gmail.com>
2025-06-06 19:46:58 -0500

    npm run webpack and added code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./styles.css";
 import { sayHello, sayGoodbye } from "./greetings.js";
+import {add} from "./mathUtils.js"
 
 console.log(sayHello("Webpack"));
 console.log(sayGoodbye("Webpack"));
+console.log(add(5,3));
+
